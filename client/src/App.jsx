0f335a54489788732dacdbd9e9cf226b819a043f
hirunikodisinghe <hirunikodisinghe@gmail.com>
2024-04-15 10:08:36 +0530

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,24 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Dashboard from './pages/Dashboard'
-import Projects from './pages/Projects'
-
+import Home from '../src/pages/Home'
+import About from '../src/pages/About'
+import Dashboard from '../src/pages/Dashboard'
+import Projects from '../src/pages/Projects'
+import Header from '../src/component/Header'
+import SignIn from '../src/pages/SignIn'
+import SignUp from '../src/pages/SignUp'
+ 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
 
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
 
       </Routes>
     </BrowserRouter>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_module/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
 
