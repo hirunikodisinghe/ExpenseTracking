3eb2079348564eef3a45588e3bc7c925be071100
hirunikodisinghe <hirunikodisinghe@gmail.com>
2024-04-16 10:30:32 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,29 @@
 import express from 'express';
-/*const express = require('express');*/
-const app = express();
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose
+
+.connect
+    (process.env.MONGO)
+.then(() => { 
+    console.log("MongoDb is connected");
+ })
+
+ .catch((err) =>{
+    console.log(err);
+ });
+
+const app = express();
+    
 app.listen(3001, () =>{
     console.log("Server is running on port 3001");
 });
 
+/*mongodb+srv://hirunikodisinghe:KAhr@123@expensetracking.apkwkj3.mongodb.net/expensetracking?retryWrites=true&w=majority*/
+/*mongodb+srv://hirunikodisinghe:<password>@expensetracking.apkwkj3.mongodb.net/?retryWrites=true&w=majority&appName=expensetracking
+mongodb+srv://hirunikodisinghe:KAhr@123@expensetracking.apkwkj3.mongodb.net/expensetracking?retryWrites=true&w=majority*/
+
+/*mongodb+srv://hirunikodisinghe:KAhr@123@expensetracking.apkwkj3.mongodb.net/expense?retryWrites=true&w=majority&appName=expensetracking*/
